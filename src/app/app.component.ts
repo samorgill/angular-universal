a29efbfa02611fd91876625df4873ecd1b4c6232
Sam Orgill <ssorgill@icloud.com>
2020-12-29 20:03:59 +0000

    check for platform

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, Inject, OnInit, PLATFORM_ID} from '@angular/core';
+import {isPlatformBrowser, isPlatformServer} from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'angular-universal';
+
+  constructor(@Inject(PLATFORM_ID) private platformId) {
+  }
+
+  ngOnInit(): void {
+    console.log('init');
+
+    if (isPlatformBrowser(this.platformId)) {
+      localStorage.setItem('name', 'sam');
+      console.log('Browser');
+      const name = localStorage.getItem('name');
+      console.log('Name ', name);
+    }
+    if (isPlatformServer(this.platformId)) {
+      console.log('Server');
+    }
+
+  }
+
 }
